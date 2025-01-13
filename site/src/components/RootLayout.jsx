05113b8f90593e795a919ddba16ed3edf1dd9aa4
Adam Winter <adamseanwinter@outlook.com>
2025-01-13 18:39:09 -0500

    add mobile nav to root layout

--- site/src/components/RootLayout.jsx
+++ site/src/components/RootLayout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import { NavBar } from './navbar/navBar';
-
+import { MobileNav } from './navbar/mobileNavBar';
 export function RootLayout({ lang, setLang }) {
   return (
     <>
       <NavBar lang={lang} setLang={setLang} />
+      <MobileNav lang={lang} setLang={setLang} />
       <Outlet />
     </>
   );
