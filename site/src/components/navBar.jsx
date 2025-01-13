57c88510cab52327a7ceb62e5819684884a367c6
Adam Winter <adamseanwinter@outlook.com>
2025-01-13 16:31:12 -0500

    desktop nav bar css responsiveness adjustments

--- site/src/components/navBar.jsx
+++ site/src/components/navBar.jsx
@@ -43,7 +43,7 @@ export function NavBar({ lang, setLang }) {
     };
 
     return (
-        <nav>
+        <nav id="desktop-nav">
             <div id="lang-select" onClick={onLangClick}>
                 {lang}
             </div>
