9830750d09b8e22b302f0031504325663b9f75cf
Adam Winter <adamseanwinter@outlook.com>
2025-01-25 20:03:32 -0500

    add file for french member info

---
+++ site/src/components/team/members/french.js
@@ -0,0 +1,125 @@
+const FRMembersInfo=[
+    {
+        "name": "Nathan",
+        "teams": ["tutoriel"],
+        "traits": {
+            "Tutoriel": 5,
+            "Années CRC": 6,
+            "Folie": 4,
+            "Efficacité": 1,
+            "Chance": 4
+        },
+        "quote": "C'est un peu fou ici"
+    },
+    {
+        "name": "Adam Clarke",
+        "teams": ["kiosque"],
+        "traits": {
+            "Construction de chaise": 10,
+            "Mathématiques": 10,
+            "Rousseur": "∞",
+            "Avoir une âme": 0
+        },
+        "quote": "Dieu m'a nerfé en me rendant roux :("
+    },
+    {
+        "name": "Sebastian Soto",
+        "teams": ["robot"],
+        "traits": {
+            "Créativité": 25,
+            "Addiction aux requins": 100,
+            "Curry": 40
+        },
+        "quote": "Je m'identifie comme C'est Nöel!"
+    },
+    {
+        "name": "Jerome Lavoie",
+        "teams": ["robot", "programmation"],
+        "traits": {
+            "Geek": "97%",
+            "Sociable": "N/A",
+            "Rideaux fermés toute la journée": 100,
+            "Utilisateur d'Arch": "👨‍💻",
+            "Peut sortir": "N/A"
+        },
+        "quote": "Si tu veux être heureuse, hypnotise-toi pour le devenir!"
+    },
+    {
+        "name": "Vlad Bretan",
+        "teams": ["robot", "site web"],
+        "traits": {
+            "Furtivité": 78,
+            "UwU": 50,
+            "Photographie": 57,
+            "ÒwÓ": 100,
+            "Fou": "J'étais fou une fois..."
+        },
+        "quote": "Alors que la nuit progress, mon cerveau ne l'est pas."
+    },
+    {
+        "name": "Safin",
+        "teams": ["programmation", "site web"],
+        "traits": {
+            "Sigma": 100,
+            "Aura": 10000,
+            "Rizz": -10000
+        },
+        "quote": "Pourquoi est-ce que je fais ça à moi-même..."
+    },
+    {
+        "name": "Eden Atlas",
+        "teams": ["vidéo"],
+        "traits": {
+            "Blender": 100,
+            "Nez": 100
+        },
+        "quote": "Nous sommes des voyants. Nous sommes des magiciens. Nous parlons en visions. Nos lettres sont comme des volées de colombes, libérées de sous nos chapeaux. Ce que nous souhaitons est à nous. Et nous, Amérique, nous sommes la nation des rêves."
+    },
+    {
+        "name": "Noah Vezina",
+        "teams": ["site web"],
+        "traits": {
+            "Procrastination": 10,
+            "Fatigue": 9,
+            "Codage": 3
+        },
+        "quote": "Je suis probablement en train d'ignorer quelque chose d'urgent"
+    },
+    {
+        "name": "Anné-Marie Andrei",
+        "teams": ["vidéo", "site web"],
+        "traits": {
+            "Santé mentale": -4,
+            "Capacité d'attention": -3,
+            "Mignonnerie": 10,
+            "Accent canadien": 98,
+            "Amateur de bugs": "♾️"
+        },
+        "quote": "Aussi confo qu'un escargot faisant dodo!!! "
+    },
+    {
+        "name": "Callahan Lowe",
+        "teams": ["robot"],
+        "traits": {
+            "Électronique": 40,
+            "Gestion de projet": 30,
+            "CAO": 35,
+            "Sarcasme": 15,
+            "Créativité": 95
+        },
+        "quote": "Ajoute des dimensions a ton CAO, sinon je vais te trouver."
+    },
+    {
+        "name": "Sandra Nitchi",
+        "teams": ["vidéo", "kiosque"],
+        "traits": {
+            "Sommeil": 0,
+            "Planification à l'avance": -5,
+            "Ruban adhésif": 100,
+            "Bonnes vibrations": 1000
+        },
+        "quote": "Je mange du sable."
+    }
+]
+
+export default FRMembersInfo
