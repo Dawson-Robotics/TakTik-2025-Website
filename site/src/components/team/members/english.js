9b3c0f6f5eb3fafaacffe486d663f56a42efe9dc
Adam Winter <adamseanwinter@outlook.com>
2025-01-25 19:57:36 -0500

    add file for english member info

---
+++ site/src/components/team/members/english.js
@@ -0,0 +1,127 @@
+const ENMembersInfo = [
+    {
+        "name": "Nathan",
+        "teams": ["tutorial"],
+        "traits": {
+            "Tutorial": 5,
+            "CRC years": 6,
+            "Silliness": 4,
+            "Efficiency": 1,
+            "Luck": 4
+        },
+        "quote": "On a scale from 1 to insane, it's not 1"
+    },
+    {
+        "name": "Adam Clarke",
+        "teams": ["kiosk"],
+        "traits": {
+            "Chair building": 10,
+            "Math": 10,
+            "Gingerness": "∞",
+            "Having a soul": 0
+        },
+        "quote": "God nerfed me by making me ginger :("
+    },
+    {
+        "name": "Sebastian Soto",
+        "teams": ["robot"],
+        "traits": {
+            "Creativity": 25,
+            "shark addiction": 100,
+            "curry": 40
+        },
+        "quote": ""
+    },
+    {
+        "name": "Jerome Lavoie",
+        "teams": ["robot", "programming"],
+        "traits": {
+            "Nerd": "97%",
+            "Sociable": "N/A",
+            "Curtains Closed All Day": 100,
+            "Arch User": "👨‍💻",
+            "Can Go Outside": "N/A"
+        },
+        "quote": "Hypnotize your way to happiness!"
+    },
+    {
+        "name": "Vlad Bretan",
+        "teams": ["robot", "website"],
+        "traits": {
+            "sneak": 78,
+            "UwU": 50,
+            "photography": 57,
+            "ÒwÓ": 100,
+            "crazy": "I was crazy once..."
+        },
+        "quote": "As the night is progressing, my brain is not."
+    },
+    {
+        "name": "Safin",
+        "teams": ["programming", "website"],
+        "traits": {
+            "sigma": 100,
+            "aura": 10000,
+            "rizz": -10000
+        },
+        "quote": "why am I doing this to myself..."
+    },
+    {
+        "name": "Eden Atlas",
+        "teams": ["video"],
+        "traits": {
+            "Blender": 100,
+            "nose": 100
+        },
+        "quote": "We are seers. We are wizards. We speak in visions. Our letters are like flocks of doves, released from under our hats. What we wish for, is ours. And we, America, we are the nation of dreams."
+    },
+    {
+        "name": "Noah Vezina",
+        "teams": ["website"],
+        "traits": {
+            "procrastination": 10,
+            "fatigue": 9,
+            "coding": 3
+        },
+        "quote": "I'm probably ignoring something urgent right now"
+    },
+    {
+        "name": "Anné-Marie Andrei",
+        "teams": ["video", "website"],
+        "traits": {
+            "Sanity": -4,
+            "Attention Span": -3,
+            "Cuteness": 10,
+            "Canadian raise": 98,
+            "bug enjoyer": "♾️"
+        },
+        "quote": "As snug as a bug in a rug!!!"
+    },
+    {
+        "name": "Callahan Lowe",
+        "teams": ["robot"],
+        "traits": {
+            "Circuitry": 40,
+            "Project Management": 30,
+            "CAD": 35,
+            "Sarcasm": 15,
+            "Creativity": 95
+        },
+        "quote": "Dimension your CAD or I will find you."
+    },
+    {
+        "name": "Sandra Nitchi",
+        "teams": ["video", "kiosk"],
+        "traits": {
+            "Sleep": 0,
+            "Planning ahead": -5,
+            "Duct tape": 100,
+            "Good Vibes": 1000
+        },
+        "quote": "I eat sand."
+    }
+]
+
+
+
+export default ENMembersInfo;
