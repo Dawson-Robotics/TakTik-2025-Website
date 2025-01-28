c72507521639cdc2db3e0de9f4ede3688be79361
Adam Winter <adamseanwinter@outlook.com>
2025-01-27 22:51:56 -0500

    add images keys for french member list

--- site/src/components/team/members/french.js
+++ site/src/components/team/members/french.js
@@ -1,4 +1,4 @@
-const FRMembersInfo=[
+const FRMembersInfo = [
     {
         "name": "Nathan",
         "teams": ["tutoriel"],
@@ -9,10 +9,12 @@ const FRMembersInfo=[
             "Efficacité": 1,
             "Chance": 4
         },
-        "quote": "C'est un peu fou ici"
+        "quote": "C'est un peu fou ici",
+        "imgUrl": "/images/team/member/Nathan Bokobza.jpg"
     },
     {
         "name": "Adam Clarke",
+        "subtitle": "Constructeur de Chaise",
         "teams": ["kiosque"],
         "traits": {
             "Construction de chaise": 10,
@@ -20,7 +22,8 @@ const FRMembersInfo=[
             "Rousseur": "∞",
             "Avoir une âme": 0
         },
-        "quote": "Dieu m'a nerfé en me rendant roux :("
+        "quote": "Dieu m'a nerfé en me rendant roux :(",
+        "imgUrl": "/images/team/member/Adam Clarke.jpg"
     },
     {
         "name": "Sebastian Soto",
@@ -30,7 +33,8 @@ const FRMembersInfo=[
             "Addiction aux requins": 100,
             "Curry": 40
         },
-        "quote": "Je m'identifie comme C'est Nöel!"
+        "quote": "Je m'identifie comme C'est Nöel!",
+        "imgUrl": "/images/team/member/Sebastian Soto.jpg"
     },
     {
         "name": "Jerome Lavoie",
@@ -42,7 +46,8 @@ const FRMembersInfo=[
             "Utilisateur d'Arch": "👨‍💻",
             "Peut sortir": "N/A"
         },
-        "quote": "Si tu veux être heureuse, hypnotise-toi pour le devenir!"
+        "quote": "Si tu veux être heureuse, hypnotise-toi pour le devenir!",
+        "imgUrl": "/images/team/member/Jérôme Lavoie.jpg"
     },
     {
         "name": "Vlad Bretan",
@@ -54,7 +59,8 @@ const FRMembersInfo=[
             "ÒwÓ": 100,
             "Fou": "J'étais fou une fois..."
         },
-        "quote": "Alors que la nuit progress, mon cerveau ne l'est pas."
+        "quote": "Alors que la nuit progress, mon cerveau ne l'est pas.",
+        "imgUrl": "/images/team/member/Vlad Bretan.jpg"
     },
     {
         "name": "Safin",
@@ -64,7 +70,8 @@ const FRMembersInfo=[
             "Aura": 10000,
             "Rizz": -10000
         },
-        "quote": "Pourquoi est-ce que je fais ça à moi-même..."
+        "quote": "Pourquoi est-ce que je fais ça à moi-même...",
+        "imgUrl": "/images/team/placeholder.jpg"
     },
     {
         "name": "Eden Atlas",
@@ -73,7 +80,8 @@ const FRMembersInfo=[
             "Blender": 100,
             "Nez": 100
         },
-        "quote": "Nous sommes des voyants. Nous sommes des magiciens. Nous parlons en visions. Nos lettres sont comme des volées de colombes, libérées de sous nos chapeaux. Ce que nous souhaitons est à nous. Et nous, Amérique, nous sommes la nation des rêves."
+        "quote": "Nous sommes des voyants. Nous sommes des magiciens. Nous parlons en visions. Nos lettres sont comme des volées de colombes, libérées de sous nos chapeaux. Ce que nous souhaitons est à nous. Et nous, Amérique, nous sommes la nation des rêves.",
+        "imgUrl": "/images/team/member/Eden Atlas.jpg"
     },
     {
         "name": "Noah Vezina",
@@ -83,7 +91,8 @@ const FRMembersInfo=[
             "Fatigue": 9,
             "Codage": 3
         },
-        "quote": "Je suis probablement en train d'ignorer quelque chose d'urgent"
+        "quote": "Je suis probablement en train d'ignorer quelque chose d'urgent",
+        "imgUrl": "/images/team/member/Noah Vezina.jpg"
     },
     {
         "name": "Anné-Marie Andrei",
@@ -95,7 +104,8 @@ const FRMembersInfo=[
             "Accent canadien": 98,
             "Amateur de bugs": "♾️"
         },
-        "quote": "Aussi confo qu'un escargot faisant dodo!!! "
+        "quote": "Aussi confo qu'un escargot faisant dodo!!! ",
+        "imgUrl": "/images/team/member/Anné-Marie Andrei.jpg"
     },
     {
         "name": "Callahan Lowe",
@@ -107,7 +117,8 @@ const FRMembersInfo=[
             "Sarcasme": 15,
             "Créativité": 95
         },
-        "quote": "Ajoute des dimensions a ton CAO, sinon je vais te trouver."
+        "quote": "Ajoute des dimensions a ton CAO, sinon je vais te trouver.",
+        "imgUrl": "/images/team/member/Callahan Lowe.jpg"
     },
     {
         "name": "Sandra Nitchi",
@@ -118,8 +129,151 @@ const FRMembersInfo=[
             "Ruban adhésif": 100,
             "Bonnes vibrations": 1000
         },
-        "quote": "Je mange du sable."
+        "quote": "Je mange du sable.",
+        "imgUrl": "/images/team/member/Sandra Nitchi.jpg"
+    },
+    {
+        "name": "Kit Nemeth",
+        "teams": ["vidéo"],
+        "traits": {
+            "BPM": 120,
+            "Gain": 11,
+            "bonne energie": 9001
+        },
+        "quote": "Je ne fais pas vraiment partie de l'équipe, je diffusais simplement de la musique là où ils tournaient la vidéo et ils n'ont pas réussi à l'éditer.",
+        "imgUrl": "/images/team/member/Kit Nemeth.jpg"
+    },
+    {
+        "name": "Tinh Ramdani",
+        "teams": ["kiosque"],
+        "traits": {
+            "diplômé": 100,
+            "Arduino": 70,
+            "travail web": 50,
+            "tres concentré": 0
+        },
+        "quote": "Non ! J'ai déjà obtenu mon diplôme ! (Je dis cela tout en continuant à revenir à Dawson Robotics)",
+        "imgUrl": "/images/team/placeholder.jpg"
+    },
+    {
+        "name": "Léa Bokobza",
+        "teams": ["vidéo", "kiosque"],
+        "traits": {
+            "Juive": "♾️",
+            "Astucieuse": 10,
+            "Flipper": "coule dans mes veines",
+            "Déteste la neige": 99
+        },
+        "quote": "Léa ne peut pas entrer dans une pièce sans faire une déclaration bruyante.",
+        "imgUrl": "/images/team/member/Léa Bokobza.jpg"
+    },
+    {
+        "name": "Erika Ellingsen",
+        "teams": ["vidéo", "kiosque", "site web"],
+        "traits": {
+            "bavardage": 1000,
+            "cerveau": 10,
+            "espièglerie": 2000,
+            "amicale": 4000
+        },
+        "quote": "Je vais renverser une table.",
+        "imgUrl": "/images/team/member/Erika Ellingsen.jpg"
+    },
+    {
+        "name": "Ioannis Pavlopoulos",
+        "teams": ["robot", "kiosque"],
+        "traits": {
+            "Créativité": 80,
+            "manipulation d'outils": 50,
+            "sommeil": 0,
+            "intelligence": 20
+        },
+        "quote": "Vous ne pouvez pas être vieux et sage si vous n'avez jamais été jeune et stupide, et nous sommes en route pour devenir des génies.",
+        "imgUrl": "/images/team/member/Ioannis Pavlopoulos.jpg"
+    },
+    {
+        "name": "Jacob Rodrigo",
+        "teams": ["robot"],
+        "traits": {
+            "Permanence de l'objet": 12,
+            "Soudure": 65,
+            "Accent western": 18,
+            "Morsure": 100,
+            "Dénuder les vis": 83
+        },
+        "quote": "Je jure que je tenais cette clé Allen il y a deux secondes...",
+        "imgUrl": "/images/team/member/Jacob Rodrigo.jpg"
+    },
+    {
+        "name": "Alexander Segal",
+        "teams": ["site web"],
+        "traits": {
+            "Charisme": 70,
+            "Intelligence": 60,
+            "Énergie": 30,
+            "Stabilité": 5,
+            "Goût musical": 100
+        },
+        "quote": "Es-tu un gars intelligent ou un gars qui sent mauvais ?",
+        "imgUrl": "/images/team/placeholder.jpg"
+    },
+    {
+        "name": "Hannah David",
+        "teams": ["vidéo", "kiosque"],
+        "traits": {
+            "bavardage": 47,
+            "gribouillages": 75,
+            "peinture": 42
+        },
+        "quote": "Un grand merci à tous mes chouchous.",
+        "imgUrl": "/images/team/placeholder.jpg"
+    },
+    {
+        "name": "Effie Polymenakos",
+        "teams": ["kiosque"],
+        "traits": {
+            "Capacités de sciage": 2,
+            "Gamer perdant": 4000000,
+            "Faits amusants obscurs": 70
+        },
+        "quote": "Il y a deux loups en moi et ils veulent s'embrasser.",
+        "imgUrl": "/images/team/member/Sam Polymenakos.jpg"
+    },
+    {
+        "name": "Mohammad Safwan Khan",
+        "teams": ["robot"],
+        "traits": {
+            "Introversion": 100
+        },
+        "quote": "Rien n'est présent, rien n'est futur, tout est passé, alors avancez toujours. Peu importe quoi !!!",
+        "imgUrl": "/images/team/member/Mohammad Safwan Khan.jpg"
+    },
+    {
+        "name": "IZZY",
+        "teams": ["robot", "vidéo", "kiosque", "programmation", "tutoriel", "site web"],
+        "traits": {
+            "Espagnol": 10,
+            "Roblox": 6,
+            "ponctualité": 0,
+            "rizz": 69
+        },
+        "quote": "Je suis là en esprit.",
+        "imgUrl": "/images/team/placeholder.jpg"
+    },
+    {
+        "name": "Adam Winter",
+        "teams": ["site web"],
+        "traits": {
+            "Factorio": 100,
+            "Site web": 20,
+            "stress": "ne peut pas être numérisé",
+            "fatigue": "oui"
+        },
+        "quote": "Quelqu'un, aidez-moi.",
+        "imgUrl": "/images/team/placeholder.jpg"
     }
 ]
+
+
 
 export default FRMembersInfo
