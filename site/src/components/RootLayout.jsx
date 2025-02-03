fbe45c32b2bb54b5d20efeb1bcad766d1286af0c
Adam Winter <adamseanwinter@outlook.com>
2025-02-02 20:31:14 -0500

    add scroll to top button

--- site/src/components/RootLayout.jsx
+++ site/src/components/RootLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import { NavBar } from './navbar/navBar';
 import { MobileNav } from './navbar/mobileNavBar';
+import { ScrollToTopButton } from './ScrollToTop';
 export function RootLayout({ lang, setLang }) {
   return (
     <>
       <NavBar lang={lang} setLang={setLang} />
       <MobileNav lang={lang} setLang={setLang} />
+      <ScrollToTopButton />
       <Outlet />
     </>
   );
